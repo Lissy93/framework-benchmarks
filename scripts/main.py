50ca1eadb41b4fd81bfe4f11762996fb2313a8ea
Alicia Sykes <alicia@omg.lol>
2025-08-09 08:06:01 +0100

    ignore python cache files

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from weather-scripts!")
+import os
 
+def main():
+    readme_path = os.path.join(os.path.dirname(__file__), 'README.txt')
+    try:
+        with open(readme_path, 'r', encoding='utf8') as f:
+            data = f.read()
+        print('\033[34m' + data)
+    except Exception as err:
+        print(f'Error reading README.txt: {err}')
+        exit(1)
 
 if __name__ == "__main__":
     main()
